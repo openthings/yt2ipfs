457d6c6a3697104e1b670ea4cb201de778072025
Nico Wehmöller <nico.wehmoeller@eripini.win>
2017-12-11 10:14:48 +0100

    Set video quality

--- yt2ipfs.js
+++ yt2ipfs.js
@@ -23,7 +23,7 @@ if (!fs.existsSync(config.output)) {
     });
 }
 
-exec(cmd, ["-g", args[0],"-f 22/137/135/244/243"], [], function (error, dlLink, stderr) {
+exec(cmd, ["-g", args[0],"-f 22/137/135"], [], function (error, dlLink, stderr) {
 
     wa = dlLink.split("https://");
 
